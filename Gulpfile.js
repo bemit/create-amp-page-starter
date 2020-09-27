3f8ee77b79e14362519268a488780c3b45df70f0
Michael Becker <michael@bemit.codes>
2020-09-27 10:33:43 +0200

    add netlify cms support with collections example #1;

--- Gulpfile.js
+++ Gulpfile.js
@@ -32,14 +32,21 @@ module.exports = ampCreator({
         // headline anchors
         '#anc-*',
         // footsnotes
-        '#fn*'
+        '#fn*',
     ],
+    collections: [{
+        data: 'src/data/blog/*.md',
+        tpl: 'src/html/blog.twig',
+        base: 'blog/',
+    }],
     twig: {
         data: {
             ampEnabled: true,
         },
         json: (file) => 'src/data/' + makePathFromFile(file) + '.json',
-        fm: (file) => 'src/data/' + makePathFromFile(file) + '.md',
+        fm: (file) => {
+            return 'src/data/' + makePathFromFile(file) + '.md'
+        },
         fmMap: (data, file) => ({
             head: {
                 title: data.attributes.title,
@@ -47,7 +54,7 @@ module.exports = ampCreator({
                 lang: data.attributes.lang,
             },
             links: {
-                canonical: makePathFromFile(file) === 'index' ? liveUrl : liveUrl + makePathFromFile(file),
+                canonical: makePathFromFile(file.path) === 'index' ? liveUrl : liveUrl + makePathFromFile(file.path),
             },
             content: renderMd(data.body),
         }),
