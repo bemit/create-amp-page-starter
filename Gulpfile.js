b32185c37ea3722251bf763b9ec92143b8f4cb17
Michael Becker <michael@bemit.codes>
2022-06-09 13:04:31 +0200

    upgrade cap-1.0.0-beta.1; restructure fmMap/NodeJS logic;

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,8 @@
 import path from 'path'
-import {ampCreator, getPageInfo, getPagesIndex, gulp} from 'create-amp-page'
-import markdownit from 'markdown-it'
-import {adjustHeadingLevel} from './markdown-it-headline-adjust.js'
-import markdownFootnote from 'markdown-it-footnote'
-import markdownAbbr from 'markdown-it-abbr'
-import markdownAnchor from 'markdown-it-anchor'
-import markdownToc from 'markdown-it-toc-done-right'
-import markdownDeflist from 'markdown-it-deflist'
-import markdownIns from 'markdown-it-ins'
-import markdownMark from 'markdown-it-mark'
+import {ampCreator, gulp} from 'create-amp-page'
 import AmpOptimizer from '@ampproject/toolbox-optimizer'
 
-const makePathFromFile = file => path.basename(file).replace('.twig', '')
+const makePathFromFile = file => path.basename(file).replace('.twig', '').replace('.md', '')
 const port = process.env.PORT || 4488
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -44,6 +35,7 @@ const pages = {
 // for infos check `create-amp-page` docs or typings/inline-doc!
 const tasks = ampCreator({
     port: port,
+    open: '/defaultPage',
     dist: 'build',
     srcMedia: 'src/media',
     distMedia: 'media',
@@ -57,10 +49,8 @@ const tasks = ampCreator({
     ],
     pages: pages,
     collections: [{
-        //data: 'src/data',
         fm: (file) => 'src/data/' + makePathFromFile(file) + '.md',
         tpl: 'src/html/pages/*.twig',
-        pagesByTpl: true,
         base: '',
         pageId: 'defaultPage',
     }, {
@@ -69,46 +59,15 @@ const tasks = ampCreator({
         base: 'blog',
         pageId: 'defaultPage',
     }],
+    data: {
+        cssInject: true,
+        ampEnabled: true,
+        injectNetlifyIdentity: false,
+    },
+    fmMap: async (data, files) => await import('./logic/fmMap.js?buster=' + new Date().getTime()).then(m => m.default(port, urls, pages, data, files)),
     twig: {
-        data: {
-            cssInject: !isDev,
-            ampEnabled: true,
-            injectNetlifyIdentity: false,
-        },
-        fmMap: (data, files) => {
-            const pageId = files.pageId
-            const pageEnv = isDev ? 'local' : 'prod'
-            const {
-                pagePath, pageBase, relPath,
-            } = getPageInfo(files, urls, pageId, pageEnv)
-            const pagesIndex = getPagesIndex(urls, pageEnv)
-            const pageData = pages[pageId]
-            return {
-                pageId: pageId,
-                styleSheets: [
-                    pageData.paths.stylesInject,
-                ],
-                head: {
-                    title: data.attributes.title,
-                    description: data.attributes.description,
-                    lang: data.attributes.lang,
-                },
-                links: {
-                    canonical: pageBase + pagePath,
-                    origin: pageBase,
-                    cdn: isDev ? 'http://localhost:' + port + '/' : pageBase,
-                    pages: pagesIndex,
-                },
-                request: {
-                    path: pagePath,
-                    // relPath,
-                },
-                hero_image: data.attributes.hero_image,
-                content: renderMd(data.body),
-            }
-        },
         logicLoader: async () => {
-            const functions = await import('./twigLogic/functions.js?buster=' + new Date().getTime()).then(m => m.default)
+            const functions = await import('./logic/functions.js?buster=' + new Date().getTime()).then(m => m.default)
             return {
                 functions,
             }
@@ -116,7 +75,7 @@ const tasks = ampCreator({
         functions: [],
     },
     watchFolders: {
-        twig: ['src/data/**/*.json', 'src/data/**/*.md', 'twigLogic/**/*.js'],
+        twig: ['src/data/**/*.json', 'src/data/**/*.md', 'logic/**/*.js'],
         sass: [],
         media: [],
     },
@@ -124,56 +83,3 @@ const tasks = ampCreator({
 })
 Object.keys(tasks).forEach(taskName => gulp.task(taskName, tasks[taskName]))
 
-const slugify = s => 'anc-' + encodeURIComponent(String(s).trim().toLowerCase().replace(/\s+/g, '-').replace(/&/g, ''))
-const md = markdownit({
-    // html: true,
-    xhtmlOut: true,
-    linkify: true,
-    typographer: true,
-})
-    .use(adjustHeadingLevel, {firstLevel: 2})
-    .use(markdownFootnote)
-    .use(markdownAbbr)
-    .use(markdownAnchor, {
-        permalink: true, permalinkBefore: true, permalinkSymbol: '#',
-        level: 3,
-        slugify,
-    })
-    .use(markdownToc, {
-        slugify,
-        level: 3,
-    })
-    .use(markdownDeflist)
-    .use(markdownIns)
-    .use(markdownMark)
-
-
-const defaultLinkRenderer = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
-    return self.renderToken(tokens, idx, options)
-}
-md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
-    const hrefRaw = tokens[idx].attrs && tokens[idx].attrs.reduce((v, prev) => prev || v[0] === 'href')
-    const href = hrefRaw ? hrefRaw[1] : ''
-    if(
-        href.indexOf('http://') === 0 ||
-        href.indexOf('https://') === 0 ||
-        href.indexOf('ftp://') === 0 ||
-        href.indexOf('ftps://') === 0
-    ) {
-        // add target blank and security attrs to any external/full url
-        tokens[idx].attrPush(['target', '_blank'])
-        tokens[idx].attrPush(['rel', 'noreferrer noopener'])
-    }
-
-    return defaultLinkRenderer(tokens, idx, options, env, self)
-}
-
-// plugin for advanced use cases:
-// https://github.com/markdown-it/markdown-it-container
-
-const renderMd = (text) => {
-    return md.render(text)
-}
-const renderInlineMd = (text) => {
-    return md.renderInline(text)
-}
